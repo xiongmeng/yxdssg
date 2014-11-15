8a5ecc4a4cce00e20c2500a8d1d78a68b6f22b16
xiongmeng <277309623@qq.com>
2014-11-15 15:08:19 +0800

    加入图片和文字

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,94 @@
 <?php
+Route::get('/', function(){
+    $products = array(
+        array(
+            'image' => '/image/CIMG1404.JPG',
+            'active' => true,
+        ),
+        array(
+            'image' => '/image/CIMG1590.JPG'
+        ),
+        array(
+            'image' => '/image/CIMG1609.JPG'
+        ),
+        array(
+            'image' => '/image/CIMG1610.JPG'
+        ),
+        array(
+            'image' => '/image/CIMG1618.JPG'
+        ),
+    );
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+    $guoMuFengCais = array(
+        array(
+            'image' => '/image/CIMG1545.JPG',
+            'active' => true,
+        ),
+        array(
+            'image' => '/image/CIMG1547.JPG'
+        ),
+        array(
+            'image' => '/image/CIMG1548.JPG'
+        ),
+        array(
+            'image' => '/image/CIMG1553.JPG'
+        ),
+        array(
+            'image' => '/image/CIMG1556.JPG'
+        ),
+        array(
+            'image' => '/image/CIMG1557.JPG'
+        ),
+    );
 
-Route::get('/', function()
-{
-	return View::make('hello');
+    $credits = array(
+        array(
+            'image' => '/image/CIMG1617.JPG',
+            'active' => true,
+        ),
+        array(
+            'image' => '/image/CIMG1573.JPG'
+        ),
+        array(
+            'image' => '/image/CIMG1582.JPG'
+        ),
+        array(
+            'image' => '/image/CIMG1643.JPG'
+        ),
+    );
+
+    $histories = array(
+         array(
+            'id' => '2011-08',
+            'title' => '注册商标家峪红',
+            'content' => '在2011年8月份我们合作社注册商标家峪红，生产无公害的水果，使果农增加了收入',
+        ),
+        array(
+            'id' => '2012-07',
+            'title' => '申请绿色食品标志',
+            'content' => '2012年7月18日我们合作社向辽宁省绿色食品办公室申请绿色食品标志 . 我们严格按照绿色食品管理条例来管理果树，使我们生产的寒富苹果达到了绿色食品标准，已通过了辽宁省生态研究所的检测。我们的苹果质量好了，可以和大的超市签订收购合同，那样果农的果品就在也不愁卖了，而且还能卖好价钱',
+        ),
+        array(
+            'id' => '2012',
+            'title' => '不断壮大的规模',
+            'content' => '截止2012年我们合作社已经发展社员1000多户，共有约近10000亩果园。合作社的成员收入的80%来源于果树。在这样的发展势头下我们更觉得责任重大， 不能辜负社员们对我们的期望，因此我们在科学合理，并与市场相对接的基础上决定通过整合土地资源，制定发展本社的发展规划，引领农民走出单一种植粮食作物，根据市场需求来耕作，以期达到大幅度提高农民收入，促进聚四方合作社的快速发展',
+        ),
+        array(
+            'id' => '2013',
+            'title' => '修建气调库',
+            'content' => '2013年我们合作社准备修建200平米的气调库，这样才能保证及时给购方充足的货源。 我们经过反复论证引导农户进行等市场前景比较看好的、口感好、价格高的新品种逐渐更换老的品种，提高市场价值种植栽培，聘请专家对农户进行栽培管理的培训，让农户得有力的经济实惠的同时也为合作社未来发展提供有力的后劲。',
+        ),
+        array(
+            'id' => '现在',
+            'title' => '服务无处不在',
+            'content' => '我们统一采购化肥、农药等生产资料；组织采购销售成员生产的水果；开展成员所需的运输、包装等服务；为成员引进新品种；提供技术培训，技术交流和咨询服务等等。给乡亲们搭建一个走向致富奔小康的平台。',
+        ),
+        array(
+            'id' => '将来',
+            'title' => '服务无处不在',
+            'content' => '要发展农村经济，想在有限的土地上创造出更多的效益必须改变现有的种植模式，合作社在资金技术产品销售等环节给予大力支持，让提早上市的无公害的、绿色的果品成为本合作社经济发展的新亮点。<br/>因此 通过我们的规划，让本乡的水果产业发展走上一个前所未有的新阶段，同时也作为立足本土 服务农民的经济组织更好发展的契机。在我们实施以上规划的同时也存在大量的难以克服的困难和问题，但我们有信心，在“精心组织求发展，热忱服务富农民”宗旨下，更加满怀信心，为我们镇的经济腾飞作出更大的贡 献。',
+        ),
+    );
+
+	return View::make('hello', array('products' => $products, 'credits' => $credits, 'guoMuFengCais' => $guoMuFengCais, 'histories' => $histories));
 });
--- app/views/hello.php
+++ app/views/hello.php
@@ -8,34 +8,15 @@
     <title>义县东升水果</title>
 
     <!-- 新 Bootstrap 核心 CSS 文件 -->
-    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <!--    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+    <script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/js/jquery.timelinr-0.9.53.js"></script>
 
 </head>
 <body id="page-top" class="index" style="background-color: #99CC01">
-<style>
-    .page-scroll a {
-        color: white !important;
-    }
-
-    hr.star-light {
-        margin: 20px auto;
-        padding: 0;
-        max-width: 400px;
-        border: 0;
-        border-top: solid 5px;
-        text-align: center;
-    }
-
-    .con-tsb{
-        margin-top: 80px;
-        z-index: 1;
-    }
-
-    section{
-        padding: 100px 0;
-    }
-</style>
-
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="marg-rig" style="background-color: darkgreen">
         <div class="navbar-header page-scroll">
@@ -47,9 +28,10 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="hidden"><a href="#page-top"></a></li>
                 <li class="hidden"><a href="#"></a></li>
-                <li class="page-scroll"><a href="#integrated">公司简介</a></li>
+                <li class="page-scroll"><a href="#product">特色产品</a></li>
                 <li class="page-scroll"><a href="#portfolio">资质荣誉</a></li>
-                <li class="page-scroll"><a href="#about">产品展示</a></li>
+                <li class="page-scroll"><a href="#guoMuFengCai">果木风采</a></li>
+                <li class="page-scroll"><a href="#integrated">发展历程</a></li>
                 <li class="page-scroll"><a href="#dig">联系我们</a></li>
             </ul>
         </div>
@@ -57,26 +39,49 @@
 </nav>
 <header>
     <div class="container">
-        <div class="row">
-            <div class="con-tsb col-lg-12"><img class="img-responsive" src="/image/tsb-bg.png" alt=""></div>
+        <div class="jumbotron con-tsb">
+            <p> 义县东升水果专业合作社成立于2010年5月，是响应上级各级党和政府的号召， 在农民自愿的基础上，由义县张家堡镇一家峪村的一些果树种植户共同组建的农民组织。合作社成立以来，
+                在'精心组织求发展，热忱服务富农民'宗旨下，取得了一些发展。广大社员在取得一定的利益后，更加满怀信心，并为合作社今后的发展献计献策，使得我们更有信心为本乡的经济腾飞作出更大的贡献，
+                义县东升水果专业合作社位于辽西锦州市义县张家堡乡一家峪村，在三零五省道路边，交通便利，通讯网络齐全。我们村三面环山，独特的地理和气候条件，适合我们大力发展果树事业。因此在合作社成立之初我们主要采取大宗农资集体团购
+                取消中间流通环节来让参加合作社的农户在投资上节约成本，农产品集体捆绑销售使得农户产品得到最大的收益的办法，使得合作社农户的收入有了明显的提高，对合作社的发展倍感信心十足。</p>
         </div>
     </div>
 </header>
-<section id="integrated" class="success">
+<section class="success" id="product">
     <div class="container">
         <div class="row">
-            <div class="col-lg-12 text-center"><h3>公司简介</h3>
-                <hr class="star-light">
+            <div class="col-lg-12 text-center"><h3>特色产品</h3>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6"><img class="img-responsive" src="/image/top.png" width="550"></div>
-            <div class="col-md-6">
-                <ul class="add text-left">
-                    绥中县杨斌果业种植专业合作社成立于2011年8月29日，位于绥中县西甸子镇北杨家村屯。由原来的7户发展到128户，已经具备了生产百万公斤优质果的能力，主要经营范围：苹果种植技术指导、储存、销售。在成立之初本着服务于社员，为客商和果农牵线搭桥，处理矛盾纠纷，打击坑农欺商行为，确保我社社员果品销售工作胜利开展。经营两年来，已经使100多户农民脱贫致富，合作客商络绎不绝。欢迎与杨斌果业专业合作社真诚的合作！
+            <div id="carousel-product" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <?php for ($index = 0; $index < count($products); $index++) { ?>
+                        <li data-target="#carousel-product" data-slide-to="<?= $index ?>"
+                            class="<?= !empty($products[$index]['active']) ? 'active' : ''; ?>"></li>
+                    <?php } ?>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <?php foreach ($products as $product) { ?>
+                        <div class="item <?= !empty($product['active']) ? 'active' : ''; ?>">
+                            <img src="<?= $product['image'] ?>" class="img-responsive" data-holder-rendered="true"/>
 
-                    　　欢迎各位有志之士前来加盟，欢迎各地客商与杨斌先生联系，前来采购苹果。本社秉承：让客商开心而来，满意而归。
-                </ul>
+                            <div class="carousel-caption">
+                                <h3>First slide label</h3>
+
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+                <a class="left carousel-control" href="#carousel-product" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-product" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
         </div>
     </div>
@@ -85,59 +90,130 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center"><h3>资质荣誉</h3>
-                <hr class="star-light">
             </div>
         </div>
-        <div class="container">
+        <div class="container" id="credit">
             <div class="row">
-                <div class="col-md-3">
-                    <div class="intro-text">
-                        <img src="/image/jiang1.jpg">
+                <div id="carousel-credit" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <?php for ($index = 0; $index < count($credits); $index++) { ?>
+                            <li data-target="#carousel-product" data-slide-to="<?= $index ?>"
+                                class="<?= !empty($credits[$index]['active']) ? 'active' : ''; ?>"></li>
+                        <?php } ?>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <?php foreach ($credits as $credit) { ?>
+                            <div class="item <?= !empty($credit['active']) ? 'active' : ''; ?>">
+                                <img src="<?= $credit['image'] ?>" class="img-responsive" data-holder-rendered="true"/>
 
-                        <p>富士苹果银奖</p>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="intro-text">
-                        <img src="/image/jiang2.jpg">
+                                <div class="carousel-caption">
+                                    <h3>First slide label</h3>
 
-                        <p>王林苹果优质奖</p>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>
+                            </div>
+                        <?php } ?>
                     </div>
+                    <a class="left carousel-control" href="#carousel-credit" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-credit" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<section class="success" id="about">
+
+<section id="guoMuFengCai">
     <div class="container">
         <div class="row">
-            <div class="col-lg-12 text-center"><h3>产品展示</h3>
-                <hr class="star-light">
+            <div class="col-lg-12 text-center"><h3>果木风采</h3>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-3">
-                <div class="intro-text">
-                    <img src="/image/jiang1.jpg">
+        <div class="container" id="guoMuFengCai">
+            <div class="row">
+                <div id="carousel-guoMuFengCai" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <?php for ($index = 0; $index < count($guoMuFengCais); $index++) { ?>
+                            <li data-target="#carousel-guoMuFengCai" data-slide-to="<?= $index ?>"
+                                class="<?= !empty($guoMuFengCais[$index]['active']) ? 'active' : ''; ?>"></li>
+                        <?php } ?>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <?php foreach ($guoMuFengCais as $guoMuFengCai) { ?>
+                            <div class="item <?= !empty($guoMuFengCai['active']) ? 'active' : ''; ?>">
+                                <img src="<?= $guoMuFengCai['image'] ?>" class="img-responsive"
+                                     data-holder-rendered="true"/>
 
-                    <p>富士苹果银奖</p>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="intro-text">
-                    <img src="/image/jiang2.jpg">
+                                <div class="carousel-caption">
+                                    <h3>First slide label</h3>
 
-                    <p>王林苹果优质奖</p>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                    <a class="left carousel-control" href="#carousel-guoMuFengCai" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-guoMuFengCai" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+<section id="integrated" class="success">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center"><h3>发展历程</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-9 col-md-offset-1">
+                <div id="timeline">
+                    <ul id="dates">
+                        <?php foreach ($histories as $history) { ?>
+                            <li><a href="#<?= 'timeline-' . $history['id'] ?>"><?= $history['id'];?></a></li>
+                        <?php } ?>
+                    </ul>
+                    <ul id="issues">
+                        <?php foreach ($histories as $history) { ?>
+                            <li id="<?= 'timeline-' . $history['id'] ?>">
+                                <h1><?= $history['title'] ?></h1>
+
+                                <p><?= $history['content'] ?></p>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
+            <!--            <div class="col-md-6"><img class="img-responsive" src="/image/top.png" width="550"></div>-->
+            <!--            <div class="col-md-6">-->
+            <!--                <ul class="add text-left">-->
+            <!--                    义县东升水果专业合作社成立于2010年5月，是响应上级各级党和政府的号召， 在农民自愿的基础上，由义县张家堡镇一家峪村的一些果树种植户共同组建的农民组织。合作社成立以来，-->
+            <!--                    在'精心组织求发展，热忱服务富农民'宗旨下，取得了一些发展。广大社员在取得一定的利益后，更加满怀信心，并为合作社今后的发展献计献策，使得我们更有信心为本乡的经济腾飞作出更大的贡献，-->
+            <!--                    义县东升水果专业合作社位于辽西锦州市义县张家堡乡一家峪村，在三零五省道路边，交通便利，通讯网络齐全。我们村三面环山，独特的地理和气候条件，适合我们大力发展果树事业。因此在合作社成立之初我们主要采取大宗农资集体团购-->
+            <!--                    取消中间流通环节来让参加合作社的农户在投资上节约成本，农产品集体捆绑销售使得农户产品得到最大的收益的办法，使得合作社农户的收入有了明显的提高，对合作社的发展倍感信心十足。-->
+            <!--                </ul>-->
+            <!--            </div>-->
+        </div>
+    </div>
+</section>
+
+
 <section id="dig">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center"><h3>联系我们</h3>
-                <hr class="star-light">
             </div>
         </div>
         <div class="row">
@@ -219,9 +295,20 @@
             class="fa fa-chevron-up"></i> </a></div>
 
 <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
-<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
-
-<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-<script src="http://cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<!--<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>-->
+<!---->
+<!--<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->-->
+<!--<script src="http://cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>-->
+<script>
+    $(function () {
+        $('#timeline').timelinr({
+            orientation: 	'vertical',
+            issuesSpeed: 	300,
+            datesSpeed: 	100,
+            arrowKeys: 		'true',
+            startAt:		3
+        });
+    });
+</script>
 </body>
 </html>
